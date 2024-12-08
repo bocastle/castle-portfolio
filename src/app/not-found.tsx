4536687ec741792f5bcc31a7fb265622ec28f3ba
bocastle <bocastle1213@gmail.com>
2024-12-09 02:06:16 +0900

    404 페이지추가

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-items-center items-center gap-8px">
+      <span>요청하신 페이지는 진행중입니다.</span>
+      <span>coming soon...</span>
+    </div>
+  );
+};
+
+export default NotFound;
