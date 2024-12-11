d385de95c2e8881ee379579f111ce8e0b5d9b667
bocastle <bocastle1213@gmail.com>
2024-12-12 02:20:53 +0900

    주석 제거

--- src/components/titles/index.tsx
+++ src/components/titles/index.tsx
@@ -1,8 +1,6 @@
 import data from "./data.json";
 const Title = () => {
-  let number = Math.floor(Math.random() * (4 - 0 + 1) + 0);
-
-  console.log(number);
+  const number = Math.floor(Math.random() * (4 - 0 + 1) + 0);
   return (
     <div className="flex bg-violet-800 h-2 sticky top-0 justify-center items-center md:static md:h-20 md:flex sm:hidden max-sm:hidden">
       {data.title.map((item) => {
