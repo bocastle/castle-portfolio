04b95603394f8c10aa43df18bd54ae19c08e8356
bocastle <bocastle1213@gmail.com>
2024-12-12 02:05:51 +0900

    max-sm 적용

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -1,55 +1,27 @@
-"use client";
-
 import Image from "next/image";
-import usePlatform from "../../hooks/usePlatform";
-const Information = () => {
-  const platform = usePlatform();
 
-  if (platform === "desktop") {
-    return (
-      <div className="flex flex-col gap-8">
-        <div className="flex md:flex items-center md:items-center mr-4 gap-6">
-          <Image
-            src={`${process.env.NEXT_PUBLIC_IMG}/7nxjpqB/image.png`}
-            width="200"
-            height="200"
-            alt={"회사명"}
-            className="object-cover rounded-lg border-[1px] border-gray-400 border-solid w-24 h-24"
-          />
-          <div className="flex flex-col gap-2">
-            <h1 className="leading-[1.15]">
-              안녕하세요,
-              <br /> 꿈을 항해하는 개발자{" "}
-              <span className="font-semibold">김보성</span>
-              입니다.
-            </h1>
-          </div>
-        </div>
+const Information = () => {
+  return (
+    <div className="justify-center justify-items-center gap-6 items-center md:flex max-sm:flex-col max-sm:justify-items-center">
+      <div>
+        <Image
+          src={`${process.env.NEXT_PUBLIC_IMG}/7nxjpqB/image.png`}
+          width="200"
+          height="200"
+          alt={"회사명"}
+          className="object-cover rounded-lg border-[1px] border-gray-400 border-solid w-24 h-24"
+        />
       </div>
-    );
-  } else {
-    return (
-      <div className="flex flex-col gap-8">
-        <div className="flex items-center flex-col">
-          <Image
-            src={`${process.env.NEXT_PUBLIC_IMG}/7nxjpqB/image.png`}
-            width="200"
-            height="200"
-            alt={"회사명"}
-            className="object-cover rounded-lg border-[1px] border-gray-400 border-solid w-24 h-24"
-          />
-          <div className="flex flex-col gap-2">
-            <h1 className="leading-[1.15] sm: text-2xl">
-              안녕하세요,
-              <br /> 꿈을 항해하는 개발자{" "}
-              <span className="font-semibold">김보성</span>
-              입니다.
-            </h1>
-          </div>
-        </div>
+      <div className="flex flex-col gap-2">
+        <h1 className="leading-[1.15] md:text-5xl sm:text-2xl max-sm:text-2xl">
+          안녕하세요,
+          <br /> 꿈을 항해하는 개발자{" "}
+          <span className="font-semibold">김보성</span>
+          입니다.
+        </h1>
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default Information;
--- src/components/titles/index.tsx
+++ src/components/titles/index.tsx
@@ -1,6 +1,6 @@
 const Title = () => {
   return (
-    <div className="flex bg-violet-800 h-2 sticky top-0 justify-center items-center md:static md:h-20 md:flex sm:hidden">
+    <div className="flex bg-violet-800 h-2 sticky top-0 justify-center items-center md:static md:h-20 md:flex sm:hidden max-sm:hidden">
       <span className="text-2xl hidden md:block font-semibold">
         No Pain, No Gain.
       </span>
