baf561c6ebd626b8c1b62b0cb545c103ba49a6e7
bocastle <bocastle1213@gmail.com>
2024-12-22 01:48:45 +0900

    navbar 홈 추가

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,18 +2,25 @@ import Link from "next/link";
 import { navLinks } from "../config/config";
 const Navbar = () => {
   return (
-    <div className="h-2 sticky top-110 md:static md:h-20 md:flex justify-end items-center gap-5 pr-12 sm: hidden">
-      {navLinks.map((item, index) => {
-        return (
-          <Link
-            key={`${item.key}${index}`}
-            href={item.href}
-            className="text-2xl md:block font-semibold"
-          >
-            {item.name}
-          </Link>
-        );
-      })}
+    <div className="h-2 sticky top-110 md:static md:h-20 md:flex justify-between items-center gap-5 px-12 sm:hidden">
+      <div>
+        <Link className="italic text-1xl md:block font-black" href={"/"}>
+          castle.log
+        </Link>
+      </div>
+      <div className="flex gap-5">
+        {navLinks.map((item, index) => {
+          return (
+            <Link
+              key={`${item.key}${index}`}
+              href={item.href}
+              className="text-2xl md:block font-semibold"
+            >
+              {item.name}
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 };
