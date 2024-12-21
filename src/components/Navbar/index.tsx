f25d8a59679cbae5b0c58d66fdb8e783b781e92c
bocastle <bocastle1213@gmail.com>
2024-12-22 02:02:40 +0900

    모바일 네비 준비

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 import { navLinks } from "../config/config";
 const Navbar = () => {
   return (
-    <div className="h-2 sticky top-110 md:static md:h-20 md:flex justify-between items-center gap-5 px-12 sm:hidden">
-      <div>
+    <div className="h-2 sticky top-110 md:static md:h-20 md:flex md:justify-between md:items-center md:gap-5 md:px-12 sm:hidden">
+      <div className="md:flex sm:hidden max-sm:hidden">
         <Link className="italic text-1xl md:block font-black" href={"/"}>
           castle.log
         </Link>
       </div>
-      <div className="flex gap-5">
+      <div className="flex gap-5 md:flex sm:hidden max-sm:hidden">
         {navLinks.map((item, index) => {
           return (
             <Link
