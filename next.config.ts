74d1f1189e8f44ba9b9022630b651aff32236e7e
bocastle <bocastle1213@gmail.com>
2024-12-23 03:02:52 +0900

    카드형식으로 수정진행중

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "i.ibb.co",
       },
+      {
+        protocol: "https",
+        hostname: "github.githubassets.com",
+      },
     ],
   },
   redirects: async () => {
--- src/components/BlogList/index.tsx
+++ src/components/BlogList/index.tsx
@@ -1,4 +1,5 @@
 import { AllArticle } from "@/app/(blogLayout)/api/types";
+import Image from "next/image";
 import Link from "next/link";
 
 interface Props {
@@ -6,13 +7,30 @@ interface Props {
 }
 
 const BlogList = ({ list }: Props) => {
-  // console.log("BlogList::", list);
+  console.log("BlogList::", list);
   return (
-    <div>
+    <div className="flex gap-2 flex-wrap justify-center">
       {list.map((item) => {
         return (
-          <Link href={`/blog/${item.pageId}`} key={item.pageId}>
-            <div className="flex flex-col gap-24">{item.title}</div>
+          <Link
+            href={`/blog/${item.pageId}`}
+            key={item.pageId}
+            className="flex flex-col items-center"
+          >
+            <Image
+              src={
+                "https://github.githubassets.com/images/mona-loading-dimmed.gif"
+              }
+              alt="thumbnail"
+              width={300}
+              height={200}
+              placeholder="blur"
+              className="hover:rounded-lg hover:border-[1.5px] hover:border-gray-400 after:hover:border-solid after:hover:transition-opacity after:hover:opacity-100 after:hover:duration-700 after:hover:ease-in-out"
+              blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg==" // 추가
+            />
+            <div className="flex flex-col gap-24 items-center">
+              {item.title}
+            </div>
           </Link>
         );
       })}
