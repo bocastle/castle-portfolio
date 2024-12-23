5d05dca714fd4864aa9f1ae851d633fe36e6cdca
bocastle <bocastle1213@gmail.com>
2024-12-24 02:59:18 +0900

    리스트 그리드로 변경

--- src/components/BlogList/index.tsx
+++ src/components/BlogList/index.tsx
@@ -1,4 +1,5 @@
 import { AllArticle } from "@/app/(blogLayout)/api/types";
+import { getDistanceFromToday, getYearMonthDay } from "@/utils/date";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -9,15 +10,15 @@ interface Props {
 const BlogList = ({ list }: Props) => {
   // console.log("BlogList::", list);
   return (
-    <div className="flex gap-2 flex-wrap justify-center">
+    <div className="grid grid-cols-2 gap-6">
       {list.map((item) => {
         return (
           <Link
             href={`/blog/${item.pageId}`}
             key={item.pageId}
-            className="flex flex-col items-center"
+            className="flex flex-col items-start gap-5"
           >
-            <div className="w-80 h-52 relative">
+            <div className="w-[476px] h-[270px] relative">
               <Image
                 unoptimized
                 loading="lazy"
@@ -30,8 +31,24 @@ const BlogList = ({ list }: Props) => {
                 fill
               />
             </div>
-            <div className="flex flex-col gap-24 items-center">
-              {item.title}
+            <div className="gap-4">
+              <div className="flex flex-col gap-24 items-start text-[24px] text-white dark:text-white">
+                {item.title}
+              </div>
+              <div className="flex flex-col gap-24 items-start text-[14px] text-indigo-300 dark:text-indigo-300">
+                {getYearMonthDay(item.createdAt)}&nbsp;&nbsp;
+                {getDistanceFromToday(item.createdAt)}
+              </div>
+              <div className="flex gap-3 pt-2 items-start">
+                {item.tagList.map((tag) => (
+                  <div
+                    key={tag.id}
+                    className="cursor-pointer text-[16px] leading-5 font-medium text-sky-600 dark:text-white bg-sky-400/10 rounded-full py-1 px-3 flex items-center  hover:bg-sky-400/20"
+                  >
+                    {tag.name}
+                  </div>
+                ))}
+              </div>
             </div>
           </Link>
         );
