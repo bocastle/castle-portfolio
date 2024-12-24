d0df5e78ebbe762c5ebebfbe7afe0611c4947728
bocastle <bocastle1213@gmail.com>
2024-12-24 15:28:28 +0900

    underline 추가

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,10 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { navLinks } from "../config/config";
 const Navbar = () => {
+  const currentPathName = usePathname();
+  console.log("currentPathName", currentPathName);
   return (
     <div className="h-2 sticky top-110 md:static md:h-20 md:flex md:justify-between md:items-center md:gap-5 md:px-12 sm:hidden">
       <div className="md:flex sm:hidden max-sm:hidden">
@@ -10,11 +14,17 @@ const Navbar = () => {
       </div>
       <div className="flex gap-5 md:flex sm:hidden max-sm:hidden">
         {navLinks.map((item, index) => {
+          const isUnderlined = currentPathName.includes(item.href);
+          console.log("isUnderlined", isUnderlined);
           return (
             <Link
               key={`${item.key}${index}`}
               href={item.href}
-              className="text-2xl md:block font-semibold"
+              className={`italic text-2xl md:block font-semibold ${
+                isUnderlined
+                  ? "underline decoration-solid underline-offset-8"
+                  : ""
+              }`}
             >
               {item.name}
             </Link>
