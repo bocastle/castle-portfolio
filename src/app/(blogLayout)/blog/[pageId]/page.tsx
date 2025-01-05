430a52c0ce1f378a4bd582782134913983981664
bocastle <bocastle1213@gmail.com>
2025-01-06 01:45:00 +0900

    generateStaticParams 추가

--- src/app/(blogLayout)/blog/[pageId]/page.tsx
+++ src/app/(blogLayout)/blog/[pageId]/page.tsx
@@ -47,20 +47,3 @@ export default async function ArticleDetailPage({ params }: Props) {
     </div>
   );
 }
-
-// // 이 함수는 Server Side에서 빌드 타임에 호출된다.
-// // Client Side에서는 호출되지 않으므로 직접 데이터베이스 쿼리를 수행할 수도 있다.
-// Next.js will invalidate the cache when a
-// request comes in, at most once every 60 seconds.
-export const revalidate = 60;
-
-// We'll prerender only the params from `generateStaticParams` at build time.
-// If a request comes in for a path that hasn't been generated,
-// Next.js will server-render the page on-demand.
-export const dynamicParams = true; // or false, to 404 on unknown paths
-
-export async function generateStaticParams({ params }: Props) {
-  const pageId = (await params).pageId;
-  const { parent } = await fetchArticlePageContent(pageId);
-  return parent;
-}
