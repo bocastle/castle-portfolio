c3ab796ffab92804fb1e9f487be16da6d73cf40a
bocastle <bocastle1213@gmail.com>
2025-01-06 01:38:31 +0900

    generateStaticParams 추가

--- src/app/(blogLayout)/blog/page.tsx
+++ src/app/(blogLayout)/blog/page.tsx
@@ -27,21 +27,9 @@ export default async function BlogPage() {
 
 // // 이 함수는 Server Side에서 빌드 타임에 호출된다.
 // // Client Side에서는 호출되지 않으므로 직접 데이터베이스 쿼리를 수행할 수도 있다.
-// export async function getStaticProps() {
-//   const List = await getPageList();
-//   // const posts = await res.json();
-
-//   // Blog 컴포넌트는 빌드 시점에 'posts'를 프로퍼티로 받는다.
-//   return {
-//     props: {
-//       List,
-//     },
-//     revalidate: 10,
-//   };
-// }
 // Next.js will invalidate the cache when a
 // request comes in, at most once every 60 seconds.
-export const revalidate = 10;
+export const revalidate = 60;
 
 // We'll prerender only the params from `generateStaticParams` at build time.
 // If a request comes in for a path that hasn't been generated,
