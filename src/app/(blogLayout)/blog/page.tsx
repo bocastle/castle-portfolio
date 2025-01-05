a312383c5165d435b411b18468637e514af7010f
bocastle <bocastle1213@gmail.com>
2025-01-06 01:30:11 +0900

    노션 리스트 수정진행중

--- src/app/(blogLayout)/blog/page.tsx
+++ src/app/(blogLayout)/blog/page.tsx
@@ -24,3 +24,31 @@ export default async function BlogPage() {
     </div>
   );
 }
+
+// // 이 함수는 Server Side에서 빌드 타임에 호출된다.
+// // Client Side에서는 호출되지 않으므로 직접 데이터베이스 쿼리를 수행할 수도 있다.
+// export async function getStaticProps() {
+//   const List = await getPageList();
+//   // const posts = await res.json();
+
+//   // Blog 컴포넌트는 빌드 시점에 'posts'를 프로퍼티로 받는다.
+//   return {
+//     props: {
+//       List,
+//     },
+//     revalidate: 10,
+//   };
+// }
+// Next.js will invalidate the cache when a
+// request comes in, at most once every 60 seconds.
+export const revalidate = 10;
+
+// We'll prerender only the params from `generateStaticParams` at build time.
+// If a request comes in for a path that hasn't been generated,
+// Next.js will server-render the page on-demand.
+export const dynamicParams = true; // or false, to 404 on unknown paths
+
+export async function generateStaticParams() {
+  const List = await getPageList();
+  return List;
+}
