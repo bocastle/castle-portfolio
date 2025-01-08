fae580b23467d353ca17e2078304225e200860d6
bocastle <bocastle1213@gmail.com>
2025-01-09 02:25:53 +0900

    햄버거 메뉴 준비

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -5,9 +5,13 @@ import { navLinks } from "../config/config";
 const Navbar = () => {
   const currentPathName = usePathname();
   // console.log("currentPathName", currentPathName);
+
+  const handleToggle = () => {
+    console.log("handleToggle 준비");
+  };
   return (
-    <div className="h-2 sticky top-110 md:static md:h-20 md:flex md:justify-between md:items-center md:gap-5 md:px-12 sm:hidden">
-      <div className="md:flex sm:hidden max-sm:hidden">
+    <div className="h-2 sticky flex items-center justify-between top-110 md:static md:h-20 md:flex md:justify-between md:items-center md:gap-5 md:px-12 max-md:h-20 max-md:px-12 max-md:flex max-md:justify-between">
+      <div className="flex md:flex">
         <Link
           className="italic text-1xl md:block text-white font-black"
           href={"/"}
@@ -15,7 +19,26 @@ const Navbar = () => {
           castle.log
         </Link>
       </div>
-      <div className="flex gap-5 md:flex sm:hidden max-sm:hidden">
+      <button
+        type="button"
+        className="m-0 p-0 md:hidden max-md:flex"
+        onClick={handleToggle}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-7 h-7 transition duration-500 stroke-black dark:stroke-white"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </button>
+      <div className="flex gap-5 md:flex sm:hidden max-md:hidden">
         {navLinks.map((item, index) => {
           const isUnderlined = currentPathName.includes(item.href);
           // console.log("isUnderlined", isUnderlined);
