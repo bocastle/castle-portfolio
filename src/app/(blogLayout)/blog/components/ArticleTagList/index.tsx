2ea80fef1f4d8035ccc817bb16fcda491279893e
bocastle <bocastle1213@gmail.com>
2025-01-09 13:39:10 +0900

    버튼 사이즈 조정

--- src/app/(blogLayout)/blog/components/ArticleTagList/index.tsx
+++ src/app/(blogLayout)/blog/components/ArticleTagList/index.tsx
@@ -8,7 +8,7 @@ export const ArticleFilterTagList = async () => {
       {articleTagList.map((tag) => (
         <div
           key={tag.id}
-          className="cursor-pointer text-[16px] leading-5 font-medium text-sky-600 dark:text-white bg-sky-400/10 rounded-full py-1 px-3 flex items-center  hover:bg-sky-400/20"
+          className="w-fit h-auto text-ellipsis cursor-pointer text-[16px] leading-5 font-medium text-sky-600 dark:text-white bg-sky-400/10 rounded-full py-1 px-3 flex items-center  hover:bg-sky-400/20"
         >
           {tag.name}
         </div>
