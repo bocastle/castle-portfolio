7a008a97e141408b099bb02958fa859ee9cc2275
bocastle <bocastle1213@gmail.com>
2025-01-15 20:09:27 +0900

    사이트맵 동적으로 생성

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,8 +1,9 @@
 const URL = "https://bocelog.vercel.app";
 
 import { MetadataRoute } from "next";
+import { getPageList } from "./(blogLayout)/api/notion";
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const routes: MetadataRoute.Sitemap = ["", "/blog", "/workHistory"].map(
     (route) => ({
       url: `${URL}${route}`,
@@ -11,14 +12,15 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.5,
     })
   );
-  // const posts: MetadataRoute.Sitemap = ["", "/blog1", "/workHistory2"].map(
-  //   (route) => ({
-  //     url: `${URL}${route}`,
-  //     lastModified: new Date().toISOString().split("T")[0],
-  //     changeFrequency: "daily",
-  //     priority: 0.5,
-  //   })
-  // );
-  console.log("[...routes]", [...routes]);
-  return [...routes];
+  const List = await getPageList();
+  const posts: MetadataRoute.Sitemap = List.map((info) => ({
+    url: `${URL}/blog/${info.pageId}`,
+    lastModified: new Date().toISOString().split("T")[0],
+    changeFrequency: "always",
+    priority: 0.5,
+  }));
+
+  // console.log("[...posts]", [...posts]);
+  // console.log("[...routes]", [...routes]);
+  return [...routes, ...posts];
 }
