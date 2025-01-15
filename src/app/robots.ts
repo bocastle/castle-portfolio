791bbfc61edb90dcf9053351b196a54154d684e0
bocastle <bocastle1213@gmail.com>
2025-01-15 15:09:26 +0900

    로봇 사이트 맵 추가

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: "/private/",
     },
     sitemap: "https://bocelog.vercel.app/sitemap.xml",
   };
--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -4,6 +4,8 @@ export default async function sitemap() {
   const routes = ["", "/blog", "/workHistory"].map((route) => ({
     url: `${URL}${route}`,
     lastModified: new Date().toISOString(),
+    changeFrequency: "daily",
+    priority: 0.5,
   }));
 
   return [...routes];
