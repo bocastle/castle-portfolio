d6a1db1f91008cd956f056829423b11ac154367c
bocastle <bocastle1213@gmail.com>
2025-01-17 01:39:39 +0900

    GTM 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Script from "next/script";
 import Navbar from "../components/Navbar";
 import "./globals.css";
 const geistSans = localFont({
@@ -40,8 +41,18 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const GTM_ID = process.env.GTM_ID;
   return (
     <html lang="ko">
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {/*  Google Tag Manager  */}
+        {`function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${GTM_ID}');`}
+        {/* End Google Tag Manager  */}
+      </Script>
       <body
         className={`bg-slate-900 ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
