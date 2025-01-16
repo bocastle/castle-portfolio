3d6b7203dc05b56d4156fa9cb9726617fbd20d24
bocastle <bocastle1213@gmail.com>
2025-01-17 01:42:46 +0900

    GTM 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,6 +60,13 @@ export default function RootLayout({
         {children}
         <SpeedInsights />
         <Analytics />
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+        {/* End Google Tag Manager (noscript)  */}
       </body>
     </html>
   );
