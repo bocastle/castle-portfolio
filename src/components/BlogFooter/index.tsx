27a7e99df1cf92a386907261c55578bfadb3c95a
bocastle <bocastle1213@gmail.com>
2025-01-22 01:15:12 +0900

    footerItem gap 추가

--- src/components/BlogFooter/index.tsx
+++ src/components/BlogFooter/index.tsx
@@ -12,7 +12,7 @@ const BlogFooter = ({ footerItem }: Props) => {
   console.log("footerItem", footerItem);
 
   return (
-    <section className="grid grid-cols-2 w-full max-w-4xl">
+    <section className="grid grid-cols-2 w-full max-w-4xl gap-3">
       {footerItem.prevArticle ? (
         <ArticleLinkCard
           articleLinkerDataType="prev"
