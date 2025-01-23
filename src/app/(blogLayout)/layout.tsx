4b5f8e7dbe916819383815ddfca7138ab6b011f9
bocastle <bocastle1213@gmail.com>
2025-01-23 10:31:45 +0900

    Infinite Scroll 추가 및 metadata 수정

--- src/app/(blogLayout)/layout.tsx
+++ src/app/(blogLayout)/layout.tsx
@@ -1,5 +1,28 @@
+import { Metadata } from "next";
 import { getArticleCategoryList } from "./api/notion";
 import { ArticleCategory } from "./components/ArticleCategory";
+export const metadata: Metadata = {
+  title: "castle log",
+  description: "개발과 일상을 공유 합니다.",
+  openGraph: {
+    title: `castle log`,
+    description: "개발과 일상을 공유 합니다.",
+    url: "https://bocelog.vercel.app",
+    siteName: "castle log",
+    locale: "ko_KR",
+    type: "website",
+    images: {
+      url: `${process.env.NEXT_PUBLIC_IMG}/7nxjpqB/image.png`,
+    },
+  },
+  verification: {
+    google: `${process.env.GOOGLE_SITE_VERIFICATION_KEY}`,
+    other: {
+      "naver-site-verification": `${process.env.NAVER_SITE_VERIFICATION_KEY}`,
+      "google-adsense-account": `${process.env.GOOGLE_ADSENSE_ACCOUNT}`,
+    },
+  },
+};
 
 export default async function BlogLayout({
   children,
@@ -8,7 +31,10 @@ export default async function BlogLayout({
 }) {
   const articleCategoryList = await getArticleCategoryList();
   return (
-    <div className="items-start mx-auto p-8 flex gap-28 my-4 mb-20 md:gap-10 md:my-4 sm:gap-5 max-lg:items-center max-md:items-center max-sm:items-center">
+    <div
+      className="items-start mx-auto p-8 flex gap-28 my-4 mb-20 md:gap-10 md:my-4 sm:gap-5 max-lg:items-center max-md:items-center max-sm:items-center"
+      id={"blog_layout_container"}
+    >
       <div className="w-1/5 items-center max-lg:hidden max-md:hidden max-sm:hidden">
         <ArticleCategory list={articleCategoryList} />
       </div>
